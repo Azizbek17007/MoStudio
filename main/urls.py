49eeb901aa3ef5f0cd65484e5edbb7615bf6de12
Azizbek <151017615+Azizbek17007@users.noreply.github.com>
2024-06-03 22:14:52 +0500

    12

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='contact'),
-    path('gallery/', gallery_page, name='gallery.html'),
+    path('gallery/', gallery_page, name='gallery'),
     path('pricing/', pricing_page, name='pricing'),
 ]
+
--- main/views.py
+++ main/views.py
@@ -6,20 +6,20 @@ def home_page(request):
     homes = Home.objects.all()
 
     context = {'homes': homes}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'context': home_page})
 
 def contact_page(request):
     contacts = Contact.objects.all()
-    return render(request, 'contact.html', {'contacts': contacts})
+    return render(request, 'contact.html', {'contacts': contact_page})
 
 def about_page(request):
     about = About.objects.all()
-    return render(request, 'about.html', {'about': about})
+    return render(request, 'about.html', {'about': about_page})
 
 def pricing_page(request):
     pricing = Pricing.objects.all()
-    return render(request, 'pricing.html', {'pricing': pricing})
+    return render(request, 'pricing.html', {'pricing': pricing_page})
 
 def gallery_page(request):
     print("Gallery page is being called")
-    return render(request, 'gallery.html')
+    return render(request, 'gallery.html', {'gallery': gallery_page})
