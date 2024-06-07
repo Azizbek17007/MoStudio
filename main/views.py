056900eab4d55485b229719ac964ac93d077accc
Azizbek <151017615+Azizbek17007@users.noreply.github.com>
2024-06-07 19:02:12 +0500

    1

--- main/views.py
+++ main/views.py
@@ -10,16 +10,20 @@ def home_page(request):
 
 def contact_page(request):
     contacts = Contact.objects.all()
+    context = {'contacts': contacts}
     return render(request, 'contact.html', {'contacts': contact_page})
 
 def about_page(request):
     about = About.objects.all()
+    context = {'about': about}
     return render(request, 'about.html', {'about': about_page})
 
 def pricing_page(request):
     pricing = Pricing.objects.all()
+    context = {'pricing': pricing}
     return render(request, 'pricing.html', {'pricing': pricing_page})
 
 def gallery_page(request):
-    print("Gallery page is being called")
+    context = {
+    'gallery': Home.objects.all()}
     return render(request, 'gallery.html', {'gallery': gallery_page})
